4edee79c19ddf228584882d3ddfcaf325383e3fc
DTS <davidtsyganov@mail.ru>
2022-02-15 16:08:34 +0700

    I separated the logic of Start and Service

--- src/library/Service.java
+++ src/library/Service.java
@@ -1,15 +1,34 @@
 package library;
 
 public class Service {
-    public static void showAllAuthors(AuthorDataAccess authors) {
+    private static AuthorDataAccess authors = new AuthorDataAccess();
+    private static BookDataAccess books = new BookDataAccess();
+
+    public static void showAllAuthors() {
         for (Author author : authors.getAuthors()) {
             System.out.println(author);
         }
     }
 
-    public static void showAllBooks(BookDataAccess books) {
+    public static void showAllBooks() {
         for (Book book : books.getBooks()) {
             System.out.println(book);
         }
+    }
+
+    public static AuthorDataAccess getAuthors() {
+        return authors;
+    }
+
+    public static void setAuthors(AuthorDataAccess authors) {
+        Service.authors = authors;
+    }
+
+    public static BookDataAccess getBooks() {
+        return books;
+    }
+
+    public static void setBooks(BookDataAccess books) {
+        Service.books = books;
     }
 }
--- src/library/Start.java
+++ src/library/Start.java
@@ -3,8 +3,6 @@ package library;
 import java.util.List;
 
 public class Start {
-    private static BookDataAccess books = new BookDataAccess();
-    private static AuthorDataAccess authors = new AuthorDataAccess();
 
     public static void main(String[] args) {
         Author author1 = new Author(1, "Алексей Марков");
@@ -12,14 +10,14 @@ public class Start {
         Book book1 = new Book(1, "Жлобология", author1);
         Book book2 = new Book(2, "Нравственные письма к Луцилию", author2);
 
-        books.add(book1);
-        books.add(book2);
+        Service.getBooks().add(book1);
+        Service.getBooks().add(book2);
 
-        authors.add(author1);
-        authors.add(author2);
+        Service.getAuthors().add(author1);
+        Service.getAuthors().add(author2);
 
-        Service.showAllAuthors(authors);
-        Service.showAllBooks(books);
+        Service.showAllAuthors();
+        Service.showAllBooks();
 
 
     }
